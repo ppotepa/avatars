170a8f0de0a490d41a274a05f169f50973c93ebf
Paweł Potępa <potepapawel@gmail.com>
2026-07-30 15:03:34 +0200

    Add deterministic 48-bit hashing utilities

--- lib/src/util/math_utils.dart
+++ lib/src/util/math_utils.dart
@@ -24,6 +24,25 @@ int fnv1a32(String text) {
 String hex32(int value) =>
     (value & 0xffffffff).toRadixString(16).padLeft(8, '0');
 
+String hash48(Iterable<int> bytes, {Iterable<int> prefix = const <int>[]}) {
+  const mask24 = 0xffffff;
+  var high = 0x811c9d;
+  var low = 0xc5a5b7;
+
+  void mix(int byte) {
+    final value = byte & 0xff;
+    high = ((high ^ value) * 0x010193) & mask24;
+    low = ((low ^ value) * 0x0001b3) & mask24;
+    high = (high + (low >> 7)) & mask24;
+    low = (low + (high >> 5)) & mask24;
+  }
+
+  for (final byte in prefix) mix(byte);
+  for (final byte in bytes) mix(byte);
+  return '${high.toRadixString(16).padLeft(6, '0')}'
+      '${low.toRadixString(16).padLeft(6, '0')}';
+}
+
 int signed32(int value) {
   final v = value & 0xffffffff;
   return v >= 0x80000000 ? v - 0x100000000 : v;
