26130216f5b7c69b710b7c0e017f424e5305b8bd
Paweł Potępa <potepapawel@gmail.com>
2026-08-05 13:52:44 +0200

    refactor(server): export bounded batch controller

--- lib/avatar_genome_server.dart
+++ lib/avatar_genome_server.dart
@@ -1,5 +1,6 @@
 library avatar_genome_server;
 
+export 'src/server/batch_http_controller.dart';
 export 'src/server/batch_resource_policy.dart';
 export 'src/server/origin_policy.dart';
 export 'src/server/server_config.dart';
