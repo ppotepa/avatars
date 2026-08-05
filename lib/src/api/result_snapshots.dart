88202b623f48d77357f854a9306b01711b0f4081
Paweł Potępa <potepapawel@gmail.com>
2026-08-05 17:37:01 +0200

    refactor(result): remove obsolete snapshot aliases

--- lib/src/api/result_snapshots.dart
+++
@@ -1,9 +0,0 @@
-import '../constraints/validation.dart';
-import '../geometry/avatar_layout.dart';
-import '../palette/avatar_palette.dart';
-
-AvatarPalette snapshotPalette(AvatarPalette source) => source;
-
-ValidationReport snapshotValidation(ValidationReport source) => source;
-
-AvatarLayout snapshotLayout(AvatarLayout source) => source;
