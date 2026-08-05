abae4f260528b3850a0f77c76921ca717329f799
Paweł Potępa <potepapawel@gmail.com>
2026-08-05 17:21:15 +0200

    fix(server): export batch artifact components

--- lib/avatar_genome_server.dart
+++ lib/avatar_genome_server.dart
@@ -1,6 +1,7 @@
 library avatar_genome_server;
 
 export 'src/server/avatar_save_repository.dart';
+export 'src/server/batch_artifact_store.dart';
 export 'src/server/batch_http_controller.dart';
 export 'src/server/batch_resource_policy.dart';
 export 'src/server/legacy_http_application.dart'
@@ -8,3 +9,4 @@ export 'src/server/legacy_http_application.dart'
 export 'src/server/origin_policy.dart';
 export 'src/server/server_config.dart';
 export 'src/server/server_request_handler.dart';
+export 'src/server/stored_zip_encoder.dart';
