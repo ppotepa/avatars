3a6695719298a2d846cc391cf875ad828cb55baa
Paweł Potępa <potepapawel@gmail.com>
2026-08-05 16:14:34 +0200

    perf(result): reuse intrinsically immutable model snapshots

--- lib/src/api/result_snapshots.dart
+++ lib/src/api/result_snapshots.dart
@@ -1,17 +1,9 @@
-import 'dart:typed_data';
-
 import '../constraints/validation.dart';
 import '../geometry/avatar_layout.dart';
-import '../geometry/avatar_layout_snapshot.dart';
 import '../palette/avatar_palette.dart';
 
-AvatarPalette snapshotPalette(AvatarPalette source) => AvatarPalette(
-      id: source.id,
-      colors: Uint32List.fromList(source.colors),
-      roles: Map<String, int>.unmodifiable(source.roles),
-    );
+AvatarPalette snapshotPalette(AvatarPalette source) => source;
 
-ValidationReport snapshotValidation(ValidationReport source) =>
-    ValidationReport(List<ValidationEntry>.unmodifiable(source.entries));
+ValidationReport snapshotValidation(ValidationReport source) => source;
 
-AvatarLayout snapshotLayout(AvatarLayout source) => snapshotAvatarLayout(source);
+AvatarLayout snapshotLayout(AvatarLayout source) => source;
