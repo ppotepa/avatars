709affd1c3dec9df08306918d5fb40e56e089e69
Paweł Potępa <potepapawel@gmail.com>
2026-08-05 17:49:51 +0200

    test(rendering): compare split atmosphere with legacy renderer

--- test/atmosphere_split_test.dart
+++ test/atmosphere_split_test.dart
@@ -1,9 +1,18 @@
-import 'package:avatar_genome/avatar_genome.dart';
+import 'package:avatar_genome/avatar_genome_advanced.dart';
+import 'package:avatar_genome/src/rendering/parts/v42_features_renderer.dart'
+    show ExtendedAtmosphereRenderer;
 import 'package:test/test.dart';
 
 void main() {
   test('split atmosphere preserves the legacy rendered appearance', () {
-    final legacy = AvatarGenerator(parts: RigClipPipeline.defaultParts);
+    final legacyParts = <AvatarPartRenderer>[
+      for (final part in RigClipPipeline.defaultParts)
+        if (part is SplitExtendedAtmosphereRenderer)
+          const ExtendedAtmosphereRenderer()
+        else
+          part,
+    ];
+    final legacy = AvatarGenerator(parts: legacyParts);
     final split = AvatarGenerator();
     final requests = <AvatarRequest>[
       AvatarRequest(
