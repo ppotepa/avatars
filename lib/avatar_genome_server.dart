d94c69d249171d17426869822b12d7b544eb7d65
Paweł Potępa <potepapawel@gmail.com>
2026-08-05 15:26:23 +0200

    feat(server): export reusable server components

--- lib/avatar_genome_server.dart
+++ lib/avatar_genome_server.dart
@@ -1,6 +1,10 @@
 library avatar_genome_server;
 
+export 'src/server/avatar_save_repository.dart';
 export 'src/server/batch_http_controller.dart';
 export 'src/server/batch_resource_policy.dart';
+export 'src/server/legacy_http_application.dart'
+    show AvatarEditorHttpApplication;
 export 'src/server/origin_policy.dart';
 export 'src/server/server_config.dart';
+export 'src/server/server_request_handler.dart';
